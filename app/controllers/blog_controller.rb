bfa949bb0b29c841a8540fb4d02ea3a22ef77c13
LƯU Thái Ngọc Thạch <1352014@student.hcmus.edu.vn>
2016-09-28 14:20:24 +0700

    Add bootstrap + templates

---
+++ app/controllers/blog_controller.rb
@@ -0,0 +1,7 @@
+class BlogController < ApplicationController
+  def home
+  end
+  
+  def post
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'blog/home', to: 'blog#home'
+  get 'blog/post', to: 'blog#post'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
