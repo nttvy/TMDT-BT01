253456c6d11dc6662a76d7344f3fad6861270bdc
LƯU Thái Ngọc Thạch <1352014@student.hcmus.edu.vn>
2016-09-30 13:39:49 +0700

    Delete root in routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,5 @@ Rails.application.routes.draw do
 
   get 'search', to: 'blogs#search', as: :search
   get 'tags/:tag', to: 'blogs#show_blog_tagged', as: :tag
-  root, to: 'blogs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
